605bd03638bcccbb6b04fa8c16a7bdc2fb8e138e
Amanda Galante <amandaglnt@gmail.com>
2024-04-02 10:32:42 -0300

    feat: Adicionando icones, fontes, roteamento

--- makefriends-core/src/app/app.routes.ts
+++ makefriends-core/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistryComponent } from './pages/registry/registry.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cadastro', component: RegistryComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  declarations: [],
+})
+export class AppRoutingModule {}
